1ee6ba841567e6f80aee6d2049ec32375f1271ab
Brandon Ramos <brandonbr.ramos@gmail.com>
2024-10-13 11:26:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    header.classList.toggle("rolagem", window.scrollY > 50);
-  });
-  
-function menuShow() {
+  function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
